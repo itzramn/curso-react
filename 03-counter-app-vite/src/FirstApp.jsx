5bee50d7e2ffc02b5a65d54af4742bfd5b8c4813
Itzel Ramon <itzelramonf@gmail.com>
2024-09-06 14:21:21 -0500

    Impresión de variables en el HTML

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,7 +1,19 @@
+const newMesage = {
+  message: "Hola Mundo",
+  name: "Itzel",
+  age: 22,
+  nickname: "itzramn",
+};
+
+const getMessage = () => {
+  return newMesage.message;
+};
+
 export const FirstApp = () => {
   return (
     <>
       <h1>Itzel</h1>
+      {/* <code>{JSON.stringify(newMesage)}</code> */}
       <p>Soy un subtitulo</p>
     </>
   );

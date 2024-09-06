027146c71a61e00aa1b64af21c0244aa30f821de
Itzel Ramon <itzelramonf@gmail.com>
2024-09-06 10:49:27 -0500

    Fetch

---
+++ 02-intro-javascript/src/bases/10-fetch.js
@@ -0,0 +1,15 @@
+const apiKey = "wqob7sxwOgN7HTTP9mNT414bqNOo4E1P";
+
+const peticion = fetch(
+  `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
+);
+
+peticion
+  .then((resp) => resp.json())
+  .then(({data}) => {
+    const {url} = data.images.original;
+    const img = document.createElement("img");
+    img.src = url;
+    document.body.append(img);
+  })
+  .catch(console.warn);

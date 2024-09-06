58ac0b1f0ceacf71809098028231ffb6bdfd8124
Itzel Ramon <itzelramonf@gmail.com>
2024-09-06 13:57:32 -0500

    Tarea: Crear un nuevo componente

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,3 @@
+export const FirstApp = () => {
+  return <h1>Itzel</h1>;
+};
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {HelloWorldApp} from "./HelloWorldApp";
+// import {HelloWorldApp} from "./HelloWorldApp";
+import {FirstApp} from "./FirstApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelloWorldApp />
+    {/* <HelloWorldApp /> */}
+    <FirstApp />
   </React.StrictMode>
 );
